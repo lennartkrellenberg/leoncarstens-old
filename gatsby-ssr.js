b415edffd4de5677e6aa0dcb5360518817f76ae2
Lennart Krellenberg <accounts@lennartkrellenberg.com>
2020-06-26 18:14:05 +0200

    Commit

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -3,9 +3,8 @@ const React = require("react")
 export const onRenderBody = ({ setHeadComponents }, pluginOptions) => {
   setHeadComponents([
     <script
-      src="https://code.jquery.com/jquery-3.4.1.min.js"
-      integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
-      crossOrigin="anonymous">
-    </script>,
+  src="https://code.jquery.com/jquery-3.5.1.min.js"
+  integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+  crossorigin="anonymous"></script>,
   ])
 }
