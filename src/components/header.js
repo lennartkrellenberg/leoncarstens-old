1f4d0dec2ab152f1a4b8d11c0ba7eaf36c14d0f3
Lennart Krellenberg <accounts@lennartkrellenberg.com>
2020-06-26 18:02:43 +0200

    Commit

--- src/components/header.js
+++ src/components/header.js
@@ -1,43 +1,47 @@
 import React from "react"
 import { Navbar, Nav } from "react-bootstrap"
 import { Link } from "gatsby"
+import $ from "jquery"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "../styles/global.css"
 import "../styles/header.css"
 import "../styles/scroll-down.css"
-import jQuery from 'jQuery'
+
 
 
 export default function Header() {
     return (
+            
             <div className="sticky-top">
-                    <Navbar expand="md" variant="light" className="nav">
-                        <Navbar.Brand href="#">Leon Carstens</Navbar.Brand>
-                        <Navbar.Toggle aria-controls="navbarReponsive" />
-                        <Navbar.Collapse id="navbarReponsive">
-                            <Nav className="ml-auto navbar" as="ul">
-                                <Nav.Item as="li" >
-                                    <Link className="nav-link" activeClassName="active" to="/potraits">Potraits</Link>
-                                </Nav.Item>
 
-                                <Nav.Item as="li" >
-                                    <Link className="nav-link" activeClassName="active" to="/events">Events</Link>
-                                </Nav.Item>
+                <Navbar expand="md" variant="light" className="nav">
+                    <Navbar.Brand href="#">Leon Carstens</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="navbarReponsive" />
+                    <Navbar.Collapse id="navbarReponsive">
+                        <Nav className="ml-auto navbar" as="ul">
+                            <Nav.Item as="li" >
+                                <Link className="nav-link" activeClassName="active" to="/potraits">Potraits</Link>
+                            </Nav.Item>
 
-                                <Nav.Item as="li" >
-                                    <Link className="nav-link" activeClassName="active" to="/produkte">Produkte</Link>
-                                </Nav.Item>
-                            </Nav>
+                            <Nav.Item as="li" >
+                                <Link className="nav-link" activeClassName="active" to="/events">Events</Link>
+                            </Nav.Item>
+
+                            <Nav.Item as="li" >
+                                <Link className="nav-link" activeClassName="active" to="/produkte">Produkte</Link>
+                            </Nav.Item>
+                        </Nav>
 
 
 
-                        </Navbar.Collapse>
+                    </Navbar.Collapse>
 
-                    </Navbar>
+                </Navbar>
 
 
 
             </div>
+
 
 
 
@@ -47,23 +51,23 @@ export default function Header() {
 
 
 (function ($) {
-    $(document).ready(function(){
-  
-      // hide .navbar first
-      $(".navbar").hide();
-  
-      // fade in .navbar
-      $(function () {
-          $(window).scroll(function () {
-  
-                   // set distance user needs to scroll before we start fadeIn
-              if ($(this).scrollTop() > 100) {
-                  $('.navbar').fadeIn();
-              } else {
-                  $('.navbar').fadeOut();
-              }
-          });
-      });
-  
-  });
-    }(jQuery));
+    $(document).ready(function () {
+
+        // hide .navbar first
+        $(".navbar").hide();
+
+        // fade in .navbar
+        $(function () {
+            $(window).scroll(function () {
+
+                // set distance user needs to scroll before we start fadeIn
+                if ($(this).scrollTop() > 100) {
+                    $('.navbar').fadeIn();
+                } else {
+                    $('.navbar').fadeOut();
+                }
+            });
+        });
+
+    });
+}($));
